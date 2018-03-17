04b5d95d313aca536d6e577159e010f7c9c8cad8
Alamdar Musavi <almusavi@gmail.com>
2018-03-17 12:34:55 -0500

    adding api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
+require 'open-uri'
 class HomeController < ApplicationController
 	def index
+		@players = open("https://api.sportradar.us/mlb/#{'SPORTRADAR_ACCESS_LEVEL_MLB'}/#{ENV['SPORTRADAR_API_VERSION_MLB']}/en/games/2017/08/23/boxscore.json?api_key=#{ENV['SPORTRADAR_API_KEY_MLB']}")
+		p "the players are #{@players}"
 	end
 end
